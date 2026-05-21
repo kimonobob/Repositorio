89c27e8314fcd827900713bc09391d38c552e8b9
Eddy Willy <yddesomar@gmail.com>
2026-05-21 14:23:33 -0500

    actualización a fondo

---
+++ data/documentos.js
@@ -0,0 +1,17 @@
+// =====================================================
+//  TODOS JUNTOS POR LA FIE - data/documentos.js
+//  Registro de Trabajos, Prácticas y Exámenes
+// =====================================================
+
+const baseTrabajos = [
+  {
+    id: 1,
+    curso: "ECO305", 
+    titulo: "Desempeño de los distritos de la Región Puno",
+    descripcion: "Trabajo final del curso Economía Regional y Urbana",
+    tipo: "trabajo", 
+    fecha: "2025-I",
+    portada: "portadas/utrabajofinaleddy.png", // 👈 ¡AQUÍ ESTÁ TU PORTADA!
+    archivo: "documentos/trabajos/trabajo final urbana.pdf" 
+  }
+];
